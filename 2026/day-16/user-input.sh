53223115cf9a8eb0d5724eb359c04fed8b7a0f02
Yoginder Bagga <yoginder.bagga@gmail.com>
2026-06-17 19:30:47 +0530

    Adding the user-input.sh script for input taking

---
+++ 2026/day-16/user-input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#	
+#	Name: Yoginder Bagga
+#	Purpose: Demonstrate input capture from the user
+#	Scope: For learning 	
+#
+#
+
+read -p "Enter your name:" name 
+read -p "Enter your age:" age
+
+echo -e  Name is "$name\n"
+echo -e Age is "$age\n"
