59036f3cc04ed9b74773de6378b4c9b0cb2c053c
Yoginder Bagga <yoginder.bagga@gmail.com>
2026-06-20 01:54:29 +0530

    added the script to write a for loop

---
+++ 2026/day-17/simple_for.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#	Name: Yoginder Bagga
+#	Scope: for learning purpose
+#	Purpose: Demonstrate for loop
+#
+
+echo -e "This program print 5 fruits name from the list\n"
+
+for fruit in orange mango apple banana papaya
+do
+	echo "Fruit name is:" $fruit
+done
