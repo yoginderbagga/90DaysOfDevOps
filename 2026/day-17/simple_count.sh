e24081b5b87e4548797ccf30e8629855ea0e4b93
Yoginder Bagga <yoginder.bagga@gmail.com>
2026-06-20 02:16:00 +0530

    Added the script simple_count.sh that print the number incrementing from 1 to 10

---
+++ 2026/day-17/simple_count.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#	Name: Yoginder Bagga
+#	Purpose: Demonstrate use of for loop range
+#	Scope: for learning purpose
+#
+
+echo -e "----SCRIPT TO DEMONSTRATE FOR LOOP------\n"
+
+for count in {1..10}
+do
+	echo "The number is:$count" 
+done
